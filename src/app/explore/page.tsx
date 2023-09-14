7ad562441fa341257f9cd3e7d0f4c7eef30a1bb0
catalyseu-r <mja97457@gmail.com>
2023-09-14 19:44:29 +0200

    before first publish

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -3,7 +3,7 @@ import Options from './Options';
 
 export default function Explore() {
   return (
-    <main className='bg-main-black w-screen min-h-screen bg-no-repeat xl:px-xlscreen md:px-mdscreen px-smscreen'>
+    <main className='bg-main-black w-screen min-h-screen bg-no-repeat xl:px-xlscreen md:px-mdscreen px-smscreen pb-24'>
       <Navbar />
       <Options />
     </main>
