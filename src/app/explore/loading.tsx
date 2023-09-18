a72c8a69ee1f2be5ca1abf73fb08b8823fc4182d
catalyseu-r <mja97457@gmail.com>
2023-09-18 21:33:34 +0200

    Working on loading states

---
+++ src/app/explore/loading.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Triangle } from 'react-loader-spinner';
+const Loading = () => {
+  return (
+    <div className='  bg-main-black w-screen h-screen  '>
+      <AnimatePresence>
+        <div className='flex justify-center items-center w-full h-full'>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <Triangle height='80%' width='80%' color='#DB7C26' ariaLabel='triangle-loading' visible={true} />
+          </motion.div>
+        </div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from 'react';
 import Navbar from '../components/Navbar';
+import Loading from '../loading';
 import Options from './Options';
 
 export default function Explore() {
   return (
-    <main className='bg-main-black w-screen min-h-screen bg-no-repeat  pb-24'>
-      <div className='lg:max-w-container-lg md:w-5/6 w-full md:px-0 px-4 mx-auto '>
-        <Navbar />
-        <Options />
-      </div>
-    </main>
+    <Suspense fallback={<Loading />}>
+      <main className='bg-main-black w-screen min-h-screen bg-no-repeat  pb-24'>
+        <div className='lg:max-w-container-lg md:w-5/6 w-full md:px-0 px-4 mx-auto '>
+          <Navbar />
+          <Options />
+        </div>
+      </main>
+    </Suspense>
   );
 }
