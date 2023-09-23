72f2042951abfd7a9b9b49fb4c8610f6c27e34f7
catalyseu-r <mja97457@gmail.com>
2023-09-24 00:07:03 +0200

    css updates

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -6,7 +6,7 @@ import Options from './Options';
 export default function Explore() {
   return (
     <Suspense fallback={<Loading />}>
-      <main className='bg-main-black w-screen  bg-no-repeat  min-h-custom-page-min  pb-24 relative'>
+      <main className='bg-main-black bg-no-repeat  min-h-custom-page-min  pb-24 relative overflow-x-hidden'>
         <Navbar />
         <div className='lg:max-w-container-lg md:w-5/6 w-full md:px-0 px-4 mx-auto '>
           <Options />
