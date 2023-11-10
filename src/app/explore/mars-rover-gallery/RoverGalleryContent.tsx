6917d3cf43509d03de94af9480905efe2b06e9c4
catalyseu-r <mja97457@gmail.com>
2023-11-10 20:32:14 +0100

    working on filter logic

--- src/app/explore/mars-rover-gallery/RoverGalleryContent.tsx
+++ src/app/explore/mars-rover-gallery/RoverGalleryContent.tsx
@@ -168,70 +168,68 @@ const RoverGalleryContent = (data: MarsRoverProfiles) => {
 
   return (
     <div className='grid lg:gap-20 md:gap-16 gap-10 pb-40 lg:mt-24 mt-20'>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className='flex md:justify-start justify-center gap-14 lg:flex-nowrap flex-wrap'>
-          <div className='grid gap-10 '>
-            <GenerateRoverIframe />
-            <div className='w-full flex gap-4 items-center'>
-              <div className='flex items-center gap-2'>
-                <RadioIcon
-                  className={`${
-                    rover?.status === 'active' ? 'text-deep-green animate-animate-ping-custom' : 'text-error-red'
-                  } text-base`}
-                />
-                <p className='flex items-center gap-1 text-base leading-6'>
-                  <span>Status: </span>
-                  <span className={`${rover?.status === 'active' ? 'text-deep-green' : 'text-error-red'} `}>
-                    {rover && rover.status}
-                  </span>
-                </p>
-              </div>
-              <div className='flex items-center gap-2'>
-                <CalendarIcon className={`text-base text-deep-green`} />
-                <p className='flex items-center gap-1 text-base leading-6'>
-                  <span className='leading-6 text-base'>Launched:</span>
-                  <span className='font-light italic leading-6 text-base'>
-                    {rover && dayjs(rover.launch_date).format('DD/MM/YYYY')}
-                  </span>
-                </p>
-              </div>
+      <div
+        key={rover?.name}
+        className='flex md:justify-start justify-center gap-14 lg:flex-nowrap flex-wrap animate-enter'
+      >
+        <div className='grid gap-10 '>
+          <GenerateRoverIframe />
+          <div className='w-full flex gap-4 items-center'>
+            <div className='flex items-center gap-2'>
+              <RadioIcon
+                className={`${
+                  rover?.status === 'active' ? 'text-deep-green animate-animate-ping-custom' : 'text-error-red'
+                } text-base`}
+              />
+              <p className='flex items-center gap-1 text-base leading-6'>
+                <span>Status: </span>
+                <span className={`${rover?.status === 'active' ? 'text-deep-green' : 'text-error-red'} `}>
+                  {rover && rover.status}
+                </span>
+              </p>
             </div>
-          </div>
-          <div className='grid grid-cols-1 items-start gap-10'>
-            <div className='flex items-center lg:gap-10 md:gap-8 gap-6 transition-all  flex-wrap'>
-              <GenerateRoverPicker data={data} />
-              <GenerateSolPicker />
-              <GenerateCameras />
-              <GenerateRecency />
-            </div>
-            <div className='py-4 self-stretch grid grid-cols-1 items-start'>
-              <label htmlFor='rover-bio' className='font-normal text-xl leading-10 text-deep-green'>
-                Bio
-              </label>
-              <div key={rover?.id} id='rover-bio' className='flex gap-[2px] flex-wrap items-center perspective-3d px-4'>
-                {rover &&
-                  findNasaSource(rover!.id, NASA_ROVERS_3D)
-                    ?.bio.split(/(\s+)/)
-                    .map((txt, index) => {
-                      return (
-                        <span
-                          key={index}
-                          style={{ animationDelay: `${index / 40}s` }}
-                          className={`animate-animate-text-custom relative transition-all origin-right opacity-0 inline-block font-light lg:text-xl text-base lg:leading-10 leading-8 text-text-white`}
-                        >
-                          {txt}
-                        </span>
-                      );
-                    })}
-              </div>
+            <div className='flex items-center gap-2'>
+              <CalendarIcon className={`text-base text-deep-green`} />
+              <p className='flex items-center gap-1 text-base leading-6'>
+                <span className='leading-6 text-base'>Launched:</span>
+                <span className='font-light italic leading-6 text-base'>
+                  {rover && dayjs(rover.launch_date).format('DD/MM/YYYY')}
+                </span>
+              </p>
             </div>
           </div>
         </div>
-      )}
-
-      <RoverPhotoGallery />
+        <div className='grid grid-cols-1 items-start gap-10'>
+          <div className='flex items-center lg:gap-10 md:gap-8 gap-6 transition-all  flex-wrap'>
+            <GenerateRoverPicker data={data} />
+            <GenerateSolPicker />
+            <GenerateCameras />
+            <GenerateRecency />
+          </div>
+          <div className='py-4 self-stretch grid grid-cols-1 items-start'>
+            <label htmlFor='rover-bio' className='font-normal text-xl leading-10 text-deep-green'>
+              Bio
+            </label>
+            <div key={rover?.id} id='rover-bio' className='flex gap-[2px] flex-wrap items-center perspective-3d px-4'>
+              {rover &&
+                findNasaSource(rover!.id, NASA_ROVERS_3D)
+                  ?.bio.split(/(\s+)/)
+                  .map((txt, index) => {
+                    return (
+                      <span
+                        key={index}
+                        style={{ animationDelay: `${index / 40}s` }}
+                        className={`animate-animate-text-custom relative transition-all origin-right opacity-0 inline-block font-light lg:text-xl text-base lg:leading-10 leading-8 text-text-white`}
+                      >
+                        {txt}
+                      </span>
+                    );
+                  })}
+            </div>
+          </div>
+        </div>
+      </div>
+      {isLoading ? <Loading /> : <RoverPhotoGallery />}
     </div>
   );
 };
