eeca7f247bc86780d7d4c74dac50a5d842e6fcc0
catalyseu-r <mja97457@gmail.com>
2023-11-21 20:03:47 +0100

    cleanup of component

--- src/app/components/ButtonCTA.tsx
+++ src/app/components/ButtonCTA.tsx
@@ -1,46 +1,15 @@
-'use client';
-
 import Link from 'next/link';
 import { ButtonPropsCTA } from '../types/buttonCTA';
 import { IoTelescopeOutline as TelescopeIcon } from 'react-icons/io5';
-import React from 'react';
 
 const ButtonCTA = ({ title: buttonText, linkTo }: ButtonPropsCTA) => {
-  const [isPause, setIsPause] = React.useState<boolean>(false);
   return (
     <Link
       href={linkTo ?? ''}
-      className={`
-       lg:py-4
-       md:py-3 
-       py-2 
-       group 
-       lg:px-6 
-       px-3  
-       
-      
-       lg:w-[16.5rem] h-auto 
-       w-auto 
-       relative 
-    overflow-hidden
-       rounded 
-       transition-all 
-       uppercase 
-       align-middle
-       cursor-pointer 
-       text-center 
-       justify-between 
-     inline-block
-        bg-gradient-to-br from-deep-green   to-interactive-green 
-      animate-animate-gradient-overlay
-
-       `}
+      className={`lg:py-4 md:py-3 py-2 group lg:px-6 px-3 lg:w-[16.5rem] h-auto w-auto relative overflow-hidden rounded transition-all uppercase cursor-pointer text-center justify-between inline-block bg-gradient-to-br from-deep-green to-interactive-green animate-animate-gradient-overlay`}
       style={{
         backgroundSize: '300% 300%',
-        animationPlayState: isPause ? 'paused' : 'running',
       }}
-      onMouseOver={() => setIsPause(true)}
-      onMouseLeave={() => setIsPause(false)}
     >
       <div className='flex items-center content-center gap-4 w-full h-full transition-all'>
         <p className='w-full lg:text-2xl text-lg  transition-all ease-linear leading-9 font-normal animate-animate-cta-text'>
